e84486774c0396b2d3dba1ef0fe7872020912708
ParadigmMango2 <jacobjhartt@gmail.com>
2025-11-01 08:42:33 -0600

    Added jenkins microservice

---
+++ services/jenkinswatcher/src/test/java/com/linkinlogs/jenkinswatcher/JenkinswatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkinlogs.jenkinswatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinswatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
