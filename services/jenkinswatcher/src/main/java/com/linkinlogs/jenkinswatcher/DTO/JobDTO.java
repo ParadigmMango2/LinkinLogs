19000502950e5e1eb1105268daa65c6ac4386fa1
ParadigmMango2 <jacobjhartt@gmail.com>
2025-11-15 13:21:26 -0700

    Improved jenkins jobDTO

--- services/jenkinswatcher/src/main/java/com/linkinlogs/jenkinswatcher/DTO/JobDTO.java
+++ services/jenkinswatcher/src/main/java/com/linkinlogs/jenkinswatcher/DTO/JobDTO.java
@@ -1,4 +1,4 @@
-package com.linkinlogs.jenkinswatcher;
+package com.linkinlogs.jenkinswatcher.DTO;
 
 import com.cdancy.jenkins.rest.domain.job.Job;
 import lombok.AllArgsConstructor;
@@ -7,21 +7,21 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class JobDTO {
     private String name;
     private String url;
     private String color;
+    private String clazz;
 
     public static JobDTO fromJob(Job job) {
-        if (job == null) {
-            return null;
-        }
-        return new JobDTO(
-            job.name(),
-            job.url(),
-            job.color()
-        );
+        if (job == null) return null;
+
+        JobDTO dto = new JobDTO();
+        dto.setName(job.name());
+        dto.setUrl(job.url());
+        dto.setColor(job.color());
+        dto.setClazz(job.clazz());
+        return dto;
     }
 }
 
--- services/jenkinswatcher/src/main/java/com/linkinlogs/jenkinswatcher/Test.java
+++ services/jenkinswatcher/src/main/java/com/linkinlogs/jenkinswatcher/Test.java
@@ -2,6 +2,7 @@ package com.linkinlogs.jenkinswatcher;
 
 import com.cdancy.jenkins.rest.JenkinsClient;
 import com.cdancy.jenkins.rest.domain.job.Job;
+import com.linkinlogs.jenkinswatcher.DTO.JobDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
