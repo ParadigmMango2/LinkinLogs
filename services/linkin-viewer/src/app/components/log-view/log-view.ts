816c330dbacba366b57d86005becd122ea956f29
Jacob Hartt <jacobjhartt@gmail.com>
2026-05-30 21:58:10 -0600

    Implement level selecting dropdown

--- services/linkin-viewer/src/app/components/log-view/log-view.ts
+++ services/linkin-viewer/src/app/components/log-view/log-view.ts
@@ -1,13 +1,19 @@
 import { Component, inject, signal } from '@angular/core';
 import { LogsService } from '../../services/logs-service';
-import { catchError } from 'rxjs';
+import { catchError, interval } from 'rxjs';
 import { LogLine } from '../../models/log-line';
 import { RangeSliderPlus } from '../range-slider-plus/range-slider-plus';
 import { FilterLogsPipe } from '../../pipes/filter-logs-pipe';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-log-view',
-  imports: [RangeSliderPlus, FilterLogsPipe],
+  imports: [
+    MatFormFieldModule, MatSelectModule, FormsModule, ReactiveFormsModule,
+    RangeSliderPlus, FilterLogsPipe
+  ],
   templateUrl: './log-view.html',
   styleUrl: './log-view.scss',
 })
@@ -15,8 +21,12 @@ export class LogView {
   logsService = inject(LogsService)
   tableHeaders = signal<string[]>([])
   log = signal<LogLine[]>([]);
+
+  // Inputs
   lineMin = signal<number>(0);
   lineMax = signal<number>(100);
+  levels = new FormControl('');
+  levelList: string[] = ['error', 'notice'];
 
   ngOnInit(): void {
     //LineId,Time,Level,Content,EventId,EventTemplate
@@ -32,6 +42,11 @@ export class LogView {
       )
       .subscribe((lines) => {
         this.log.set(lines);
+      });
+
+    interval(1000)
+      .subscribe(() => {
+        console.log(this.levels.value);
       });
   }
 }
